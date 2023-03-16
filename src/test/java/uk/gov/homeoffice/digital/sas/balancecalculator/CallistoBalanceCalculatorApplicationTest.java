5f88124b713f8c70c35b19f349e76d29838ad625
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2023-03-16 14:53:42 +0000

    add context loads test

---
+++ src/test/java/uk/gov/homeoffice/digital/sas/balancecalculator/CallistoBalanceCalculatorApplicationTest.java
@@ -0,0 +1,18 @@
+package uk.gov.homeoffice.digital.sas.person;
+
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+class CallistoPersonRestApiApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
