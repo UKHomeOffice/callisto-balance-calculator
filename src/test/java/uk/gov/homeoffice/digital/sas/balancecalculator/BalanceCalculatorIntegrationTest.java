9739a0c15d4b280e3795da4ffbc4d7ced7d3e0de
Seweryn <seweryn.michota@digital.homeoffice.gov.uk>
2023-05-01 15:46:33 +0100

    Add integration test for 2 days TimeEntry span

--- src/test/java/uk/gov/homeoffice/digital/sas/balancecalculator/BalanceCalculatorIntegrationTest.java
+++ src/test/java/uk/gov/homeoffice/digital/sas/balancecalculator/BalanceCalculatorIntegrationTest.java
@@ -1,10 +1,5 @@
 package uk.gov.homeoffice.digital.sas.balancecalculator;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.homeoffice.digital.sas.balancecalculator.utils.TestUtils.createTimeEntry;
-
-import java.time.ZonedDateTime;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +7,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import uk.gov.homeoffice.digital.sas.balancecalculator.models.accrual.Accrual;
 import uk.gov.homeoffice.digital.sas.balancecalculator.models.timecard.TimeEntry;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.homeoffice.digital.sas.balancecalculator.utils.TestUtils.createTimeEntry;
 
 @SpringBootTest
 @AutoConfigureWireMock(port = 9999)
@@ -42,6 +43,26 @@ class BalanceCalculatorIntegrationTest {
     List<Accrual> accruals = balanceCalculator.calculate(timeEntry);
 
     assertThat(accruals).hasSize(4);
+  }
+
+  @Test
+  void calculate_accrualsDays_whenTimeEntryHasTwoDaysSpan() {
+
+    String timeEntryId = "7f000001-879e-1b02-8187-9ef1640f0013";
+    String tenantId = "52a8188b-d41e-6768-19e9-09938016342f";
+    String personId = "0936e7a6-2b2e-1696-2546-5dd25dcae6a0";
+    ZonedDateTime startTime = ZonedDateTime.parse("2023-04-22T22:00:00+00:00");
+    ZonedDateTime finishTime = ZonedDateTime.parse("2023-04-23T06:00:00+00:00");
+
+    TimeEntry timeEntry = createTimeEntry(timeEntryId,
+        tenantId,
+        personId,
+        startTime,
+        finishTime);
+
+    List<Accrual> accruals = balanceCalculator.calculate(timeEntry);
+
+    assertThat(accruals).hasSize(2);
   }
 
 }
