5820ff3227582aee6341813591573fa1e3465406
Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>
2023-05-23 20:47:30 +0100

    Remove unused code

--- src/main/java/uk/gov/homeoffice/digital/sas/balancecalculator/module/AccrualModule.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/balancecalculator/module/AccrualModule.java
@@ -4,12 +4,10 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.SortedMap;
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 import uk.gov.homeoffice.digital.sas.balancecalculator.models.accrual.enums.AccrualType;
 import uk.gov.homeoffice.digital.sas.balancecalculator.models.timecard.TimeEntry;
 
 @Getter
-@Slf4j
 public abstract class AccrualModule {
 
   protected AccrualType accrualType;
