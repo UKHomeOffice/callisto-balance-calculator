7d09803dd21fa553bc70caaf89e80690c700db39
Sev <seweryn.michota@digital.homeoffice.gov.uk>
2023-06-16 16:41:44 +0100

    Add tests for moving TimeEntry start date to the future and past.

--- src/test/java/uk/gov/homeoffice/digital/sas/balancecalculator/BalanceCalculatorUpdateActionIntegrationTest.java
+++ src/test/java/uk/gov/homeoffice/digital/sas/balancecalculator/BalanceCalculatorUpdateActionIntegrationTest.java
@@ -120,6 +120,24 @@ class BalanceCalculatorUpdateActionIntegrationTest {
   }
 
   @Test
+  void calculate_updateNightHoursToOneDaySpan_contributionsAndCumulativeTotalsAsExpected() {
+
+    TimeEntry timeEntry = createTimeEntry(TIME_ENTRY_ID,
+        TENANT_ID,
+        PERSON_ID,
+        "2023-04-22T22:00:00+01:00",
+        "2023-04-22T23:30:00+01:00");
+
+    List<Accrual> accruals = balanceCalculator.calculate(timeEntry, KafkaAction.UPDATE);
+
+    assertThat(accruals).hasSize(6);
+
+    assertTypeAndDateAndTotals(accruals.get(3), NIGHT_HOURS, "2023-04-22", 30, 1030);
+    assertTypeAndDateAndTotals(accruals.get(4), NIGHT_HOURS, "2023-04-23", 60, 1090);
+    assertTypeAndDateAndTotals(accruals.get(5), NIGHT_HOURS, "2023-04-24", 120, 1210);
+  }
+
+  @Test
   void calculate_updateTimeEntryToPast_contributionsAndCumulativeTotalsAsExpected() {
     TimeEntry timeEntry = createTimeEntry("cc01b98e-12d4-4b06-8941-9919a2db45a9",
         TENANT_ID,
@@ -135,6 +153,22 @@ class BalanceCalculatorUpdateActionIntegrationTest {
   }
 
   @Test
+  void calculate_updateTimeEntryToPast_nightHoursContributionsAndCumulativeTotalsAsExpected() {
+    TimeEntry timeEntry = createTimeEntry("cc01b98e-12d4-4b06-8941-9919a2db45a9",
+        TENANT_ID,
+        PERSON_ID,
+        "2023-03-22T01:00:00+00:00",
+        "2023-03-22T04:00:00+00:00");
+
+    List<Accrual> accruals = balanceCalculator.calculate(timeEntry, KafkaAction.UPDATE);
+
+    assertTypeAndDateAndTotals(accruals.get(5), NIGHT_HOURS, "2023-03-22", 180, 180);
+    assertTypeAndDateAndTotals(accruals.get(6), NIGHT_HOURS, "2023-03-23", 420, 600);
+    assertTypeAndDateAndTotals(accruals.get(7), NIGHT_HOURS, "2023-03-24", 0, 600);
+  }
+
+
+  @Test
   void calculate_updateTimeEntryToFuture_contributionsAndCumulativeTotalsAsExpected() {
     TimeEntry timeEntry = createTimeEntry("02e78cf4-8467-4d10-9816-c18f646c9061",
         TENANT_ID,
@@ -145,12 +179,29 @@ class BalanceCalculatorUpdateActionIntegrationTest {
     List<Accrual> accruals = balanceCalculator.calculate(timeEntry, KafkaAction.UPDATE);
 
     assertTypeAndDateAndTotals(accruals.get(0), ANNUAL_TARGET_HOURS, "2023-03-23", 420, 8520);
-
     assertTypeAndDateAndTotals(accruals.get(1), ANNUAL_TARGET_HOURS, "2023-03-24", 300, 8820);
-
     assertTypeAndDateAndTotals(accruals.get(2), ANNUAL_TARGET_HOURS, "2023-03-25", 0, 8820);
     assertTypeAndDateAndTotals(accruals.get(3), ANNUAL_TARGET_HOURS, "2023-03-26", 120, 8940);
     assertTypeAndDateAndTotals(accruals.get(4), ANNUAL_TARGET_HOURS, "2023-03-27", 60, 9000);
 
   }
+
+  @Test
+  void calculate_updateTimeEntryToFuture_nightHoursContributionsAndCumulativeTotalsAsExpected() {
+    TimeEntry timeEntry = createTimeEntry("02e78cf4-8467-4d10-9816-c18f646c9061",
+        TENANT_ID,
+        PERSON_ID,
+        "2023-03-26T00:00:00+00:00",
+        "2023-03-26T00:35:00+00:00");
+
+    List<Accrual> accruals = balanceCalculator.calculate(timeEntry, KafkaAction.UPDATE);
+
+    assertTypeAndDateAndTotals(accruals.get(5), NIGHT_HOURS, "2023-03-23", 420, 420);
+    assertTypeAndDateAndTotals(accruals.get(6), NIGHT_HOURS, "2023-03-24", 0, 420);
+    assertTypeAndDateAndTotals(accruals.get(7), NIGHT_HOURS, "2023-03-25", 0, 420);
+    assertTypeAndDateAndTotals(accruals.get(8), NIGHT_HOURS, "2023-03-26", 35, 455);
+    assertTypeAndDateAndTotals(accruals.get(9), NIGHT_HOURS, "2023-03-27", 0, 455);
+
+  }
+
 }
